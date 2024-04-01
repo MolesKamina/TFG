5383369458242ac9eaf15f2256bf2a48ab5f5c34
MolesKamina <miguelmoles@correo.ugr.es>
2024-04-01 22:57:23 +0200

    Modificación de la clase MessageClient. Esta clase será la que se sobreescriba con las clases Producer y Consumer con el servicio de comunicación que se desee utilizar

--- message_client.py
+++ message_client.py
@@ -1,20 +1,37 @@
-# message_client.py
-# En esta clase se va a gestionar el envío y recepción de mensajes.
-# Únicamente se encargará de gestionar ese "puente" entre todos los actores
-# Tendrá dos métodos principales, aunque se podrán programar más a conveniencia
-# El método send_message se encargará de enviar un mensaje, y el método receive_message de recibirlo
-# Internamente se programará para que se utilice un servidor externo para gestionar este servicio
-
 class MessageClient:
     def __init__(self, topic_list, bootstrap_servers, enable_auto_commit):
+        """
+        Inicializa un nuevo cliente de mensajes.
+
+        Args:
+            topic_list (list): Lista de topics a los que el cliente se suscribirá.
+            bootstrap_servers (str): Lista de servidores para establecer la conexión.
+            enable_auto_commit (bool): Indicador para habilitar o deshabilitar el commit automático de offsets.
+        """
         self.topic_list = topic_list
         self.bootstrap_servers = bootstrap_servers
         self.enable_auto_commit = enable_auto_commit
 
     def send_message(self, topic, message_content):
+        """
+        Envía un mensaje al topic especificado.
+
+        Args:
+            topic (str): El topic al que se enviará el mensaje.
+            message_content (str): El contenido del mensaje a enviar.
+        """
         # Método para enviar mensajes
         pass
 
     def receive_message(self, topic):
+        """
+        Recibe un mensaje del topic especificado.
+
+        Args:
+            topic (str): El topic del cual se quiere recibir el mensaje.
+
+        Returns:
+            str: El contenido del mensaje recibido.
+        """
         # Método para recibir mensajes
         pass
