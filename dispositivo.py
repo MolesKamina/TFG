edbabac4087a116c84f7b4ebed229a838cbd60f7
MolesKamina <miguelmoles@correo.ugr.es>
2024-04-01 22:37:31 +0200

    Modificación de la clase Dispositivo. Ahora tiene comentarios que ayudan a entender mejor su funcionamiento.

--- dispositivo.py
+++ dispositivo.py
@@ -2,4 +2,11 @@ from nodo import Nodo
 
 class Dispositivo:
     def __init__(self, nodo: Nodo):
+        """
+        Inicializa un nuevo dispositivo asociado a un nodo. La clase Dispositivo es la clase encargada de gestionar
+        el apartado de nodos en el sistema.
+
+        Args:
+            nodo (Nodo): El nodo al que está asociado el dispositivo.
+        """
         self.nodo = nodo
