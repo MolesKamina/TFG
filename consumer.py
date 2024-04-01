86e284f3a44705854e72cc8937fb1edcebb7839b
MolesKamina <miguelmoles@correo.ugr.es>
2024-04-01 22:21:02 +0200

    Modificación de la clase Consumer. Ahora cada método tiene un comentario útil para entender mejor su funcionamiento

--- consumer.py
+++ consumer.py
@@ -4,6 +4,14 @@ from json import loads
 
 class Consumer(MessageClient):
     def __init__(self, topic_list, bootstrap_servers, enable_auto_commit):
+        """
+        Inicializa un nuevo consumidor de Kafka.
+
+        Args:
+            topic_list (list): Lista de topics a los que el consumidor se suscribirá.
+            bootstrap_servers (str): Lista de servidores de Kafka para establecer la conexión.
+            enable_auto_commit (bool): Indicador para habilitar o deshabilitar el commit automático de offsets.
+        """
         super().__init__(topic_list, bootstrap_servers, enable_auto_commit)
         self.consumer = KafkaConsumer(
             *topic_list,
@@ -13,6 +21,15 @@ class Consumer(MessageClient):
         )
 
     def receive_message(self, topic):
+        """
+        Recibe un mensaje del topic especificado.
+
+        Args:
+            topic (str): El topic del cual se quiere recibir el mensaje.
+
+        Returns:
+            message: El mensaje recibido.
+        """
         for message in self.consumer:
             if message.topic == topic:
                 return message.value
