9c03b64a50834b759ea6b423a533b4af3a212c1e
MolesKamina <miguelmoles@correo.ugr.es>
2024-06-05 14:09:15 +0200

    modificación de definicion

--- dispositivo.py
+++ dispositivo.py
@@ -4,8 +4,8 @@ from nodo import Nodo
 class Dispositivo:
     def __init__(self, id, description, nodo: Nodo=None):
         """
-        Inicializa un nuevo dispositivo asociado a un nodo. La clase Dispositivo es la clase encargada de gestionar
-        el apartado de nodos en el sistema.
+        Inicializa un nuevo dispositivo asociado a un nodo. La clase Dispositivo es la clase 
+        encargada de gestionar el apartado de nodos en el sistema.
 
         Args:
             nodo (Nodo): El nodo al que está asociado el dispositivo.
